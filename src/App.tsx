bec500839b06e6c2e05e95125a33c2a98a3e853e
dmasiii <danilomasi999@gmail.com>
2024-03-04 21:41:34 +0100

    Describify

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+//Components
+import GeneratePage from "./pages/GeneratePage";
+import HomePage from "./pages/HomePage";
+
+export default function App() {
+  return (
+    <>
+      <GeneratePage />
+    </>
+  )
+}
